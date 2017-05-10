184276b14854e8809c31473be36d74ac2313bbba
Benjamin Brown <benjamin.t.brown@hotmail.com>
2017-05-10 17:58:03 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var core = require( './core' );
+
+core.say( [
+  'He went to the door.',
+  'He opened the door.',
+  'And suddenly he saw...'
+], () =>{
+  core.choose( 'What do you want to do next?', [ {
+    text: 'Wait, what?  Tell me what happened, you dimwit!',
+    cb: () => {
+      core.say( 'nah', process.exit.bind( process, 0 ) );
+    }
+  } ] );
+} );
+
